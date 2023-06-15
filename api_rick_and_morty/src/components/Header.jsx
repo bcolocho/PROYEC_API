58411650b03648d8071ae52fa373d9bdbeea6877
bcolocho <127900656+bcolocho@users.noreply.github.com>
2023-06-15 01:45:54 -0600

    creando header

---
+++ api_rick_and_morty/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import Personajes from './Personajes'
+import Creadores from './Api creadores'
+
+export default function Header() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/personajes' element={<Personajes />}/>
+                <Route path='/api creadores' element={<Creadores />}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
