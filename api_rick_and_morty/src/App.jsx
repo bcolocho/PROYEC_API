bad35a4ea6db3ee8068e03a71d97a07058fc20eb
alejaan <alejan03lina@outlook.es>
2023-06-19 18:25:58 -0600

    trabajando con el login

--- api_rick_and_morty/src/App.jsx
+++ api_rick_and_morty/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header'
 import './assets/css/style.css'
+import Login from './Components/Login'
 
 function App() {
   
   return (
     <>
+      <Login />
       <Header />
     </>
   )
