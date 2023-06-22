1b55e8d18d831daf110f352d95437374988eb39c
bcolocho <127900656+bcolocho@users.noreply.github.com>
2023-06-21 18:11:07 -0600

    cambios pagina personajes

--- api_rick_and_morty/src/components/Personajes.jsx
+++ api_rick_and_morty/src/components/Personajes.jsx
@@ -1,16 +1,18 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import paginations from './components/paginations'
 
 export default function Personajes() {
   const [Personage, setPersonage] = useState([]);
+  let [pagNumber, setPageNumber] = useState(1);
   const url = "https://rickandmortyapi.com/api/character";
 
   const obtenerPersonage = (url) => {
     axios.get(url).then((response) => {
-        setPersonage(response.data.results);
-     }).catch((error) => {
-        console.log(error);
-      });
+      setPersonage(response.data.results);
+    }).catch((error) => {
+      console.log(error);
+    });
   };
 
   useEffect(() => {
@@ -18,34 +20,37 @@ export default function Personajes() {
   }, []);
 
   return (
-    <div className='container'>    
-          <h1 className='enlace fs-4 fw-bold text-center mt-4 text-white'>PERSONAJES</h1> 
-          <div className='row my-5'>
-                {
-                 Personage.map((Person, index) => {                  
-                      return (
-                          
-                          <div className='col-lg-3 col-md-6 col-sm-12 my-4' key={index}>
-                              <div className="card enlace">
-                                  <div className='face front'>
-                                     <img src={Person.image} className="img-fluid" alt="person"/> 
-                                    <h5>{Person.name}</h5> 
-                                  </div>                                   
-                                  <div className='face back'>                                       
-                                        <h5>Detalles</h5> 
-                                        <h5>{Person.name}</h5>                                                                  
-                                        <p>Status: {Person.status}</p>                             
-                                        <p>Species: {Person.species}</p>                        
-                                        <p>Gender: {Person.gender}</p>
-                                        <p>Origin: {Person.origin.name}</p>
-                                        <p>Location: {Person.location.name}</p> 
-                                  </div>                                                          
-                              </div>                                 
-                          </div>    
-                      )  
-                  }) 
-                }
-          </div>
+    <div className='container'>
+      <h1 className='enlace fs-4 fw-bold text-center mt-4 text-white'>PERSONAJES</h1>
+      <div className='row my-5'>
+        {
+          Personage.map((Person, index) => {
+            return (
+
+              <div className='col-lg-3 col-md-6 col-sm-12 my-4' key={index}>
+                <div className="card">
+                  <div className='face front'>
+                    <img src={Person.image} className="img-fluid" alt="person" />
+                    <h5>{Person.name}</h5>
+                  </div>
+                  <div className='face back'>
+                    <h5>Detalles</h5>
+                    <h5>{Person.name}</h5>
+                    <p>Status: {Person.status}</p>
+                    <p>Species: {Person.species}</p>
+                    <p>Gender: {Person.gender}</p>
+                    <p>Origin: {Person.origin.name}</p>
+                    <p>Location: {Person.location.name}</p>
+                  </div>
+                </div>
+              </div>
+
+
+
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
