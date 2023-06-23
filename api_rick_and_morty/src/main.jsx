4662abafe371dc922f27983dbaff63eb78e053db
bcolocho <127900656+bcolocho@users.noreply.github.com>
2023-06-22 20:56:14 -0600

    actualización main

--- api_rick_and_morty/src/main.jsx
+++ api_rick_and_morty/src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     redirectUri={window.location.origin}>
 
     <React.StrictMode>
-      <App_boris />
+      <App />
     </React.StrictMode>,
 
   </Auth0Provider>
