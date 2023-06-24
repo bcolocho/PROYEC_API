42d5e19d69c311de144ffe3ebd527940bee5bcad
bcolocho <127900656+bcolocho@users.noreply.github.com>
2023-06-23 18:36:37 -0600

    Update main.jsx

--- api_rick_and_morty/src/main.jsx
+++ api_rick_and_morty/src/main.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { Auth0Provider } from '@auth0/auth0-react'
-
+import App_boris from './App_boris.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <Auth0Provider
-    domain='dev-kgt5vb8fp662jhjf.us.auth0.com'
-    clientId='Jug1P7M6nqIPXHaA3plLhEJqUrfDM1XT'
-    redirectUri={window.location.origin}>
-
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-
-  </Auth0Provider>
+  <React.StrictMode>
+    <App_boris />
+  </React.StrictMode>,
 )
