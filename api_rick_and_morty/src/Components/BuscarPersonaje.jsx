6edb38b2bd1c00e2df331ce6ba203821027c929c
Erick Vásquez <65610711+topsi-vas@users.noreply.github.com>
2023-06-24 12:57:09 -0600

    finalizando componente buscar personaje
    este cambio permite ver los cambios finales sobre el filtrado de los personajes

--- api_rick_and_morty/src/Components/BuscarPersonaje.jsx
+++ api_rick_and_morty/src/Components/BuscarPersonaje.jsx
@@ -5,47 +5,66 @@ export default function BuscarPersonaje() {
 
 const[personajes, setPersonajes]=useState([]);
 const[name, setName]=useState("");
+const[estados, setEstados]=useState("");
 //funcion para obtener el nombre del personaje
 const obtenerPersonajeByNombre=()=>{
-    axios.get(`https://rickandmortyapi.com/api/character/?name=${name}`).then((response)=>{
-        //console.log(response.data.results); 
-        setPersonajes(response.data.results);
-    }).catch((error)=>{
-        console.log(error);
-    })
-}
+    console.log(name.length);
+    console.log(name);
+    
+        axios.get(`https://rickandmortyapi.com/api/character/?name=${name}&status=${estados}`).then((response)=>{
+            //console.log(response.data.results); 
+            setPersonajes(response.data.results);
+        }).catch((error)=>{
+            setPersonajes([]);
+           
+            console.log(error);
+        })
+
+  }
 
 //recibiendo los valores
 const handleName=(e)=>{
-  
-    setName(e.target.value);
-
+        setName(e.target.value) 
+}
+const handleClick=(e)=>{
+    console.log(e.target.value);
+    setEstados(e.target.value);  
 }
 const handleSubmit=(e)=>{
   obtenerPersonajeByNombre();
   e.preventDefault();
 }
-//console.log(personajes);
+
+   
+
+console.log(personajes);
   return (
     <div className='text-white'>
         <div className="container">
             <h1 className='text-center pb-2'>Encuentra a tu Personaje Favorito</h1>
             <div className="row pb-2">
-                <div className="col-md-6">
+                <div className="col-sm-12 col-md-6">
                     <form onSubmit={handleSubmit}>
                         <label htmlFor="" className='pb-2'>Ingrese el nombre del personaje</label>
                         <input type="text" className='form-control mb-2' onChange={handleName}/>
+                        
+                        <label htmlFor="" className='me-3'>Seleccione el estado del personaje</label>
+                        <select name="" id="" className='me-3' onChange={handleClick}>
+                            <option value="alive">VIVO</option>
+                            <option value="dead">MUERTO</option>
+                            <option value="unknown">DESCONOCIDO</option>
+                        </select>
                         <button  className="btn btn-primary" type='submit'>Ver Personaje </button>
                     </form>
                 </div>
-                <div className="col-md-6 d-flex align-items-center justify-content-center">
-                    <label htmlFor="" className='me-3'>Seleccione el estado del personaje</label>
+                <div className="col-sm-12 col-md-6 d-flex align-items-center justify-content-center">
+                    {/* <label htmlFor="" className='me-3'>Seleccione el estado del personaje</label>
                     <select name="" id="" className='me-3'>
-                        <option value="VIVO">VIVO</option>
-                        <option value="MUERTO">MUERTO</option>
-                        <option value="DESCONOCIDO">DESCONOCIDO</option>
-                    </select>
-                    <button  className="btn btn-primary" type='submit'>Ver Personaje </button>
+                        <option value="alive">VIVO</option>
+                        <option value="dead">MUERTO</option>
+                        <option value="unknown">DESCONOCIDO</option>
+                    </select> */}
+                    {/* <button  className="btn btn-primary boton" type='submit' onClick={handleClick}>Ver Personaje </button> */}
                 </div>
             </div>
             {/**ingreso de personajes */}
