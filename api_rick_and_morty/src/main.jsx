2a5f1f891a025a9fb1fdff631594124733c5c3b3
alejaan <alejan03lina@outlook.es>
2023-06-25 14:57:07 -0600

    actualizando componentes

--- api_rick_and_morty/src/main.jsx
+++ api_rick_and_morty/src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App_boris from './App_boris.jsx'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Auth0Provider
+  domain='dev-kgt5vb8fp662jhjf.us.auth0.com'
+  clientId='Jug1P7M6nqIPXHaA3plLhEJqUrfDM1XT'
+  redirectUri={window.location.origin}>
+
   <React.StrictMode>
-    <App_boris />
+    <App />
   </React.StrictMode>,
+
+</Auth0Provider>
 )
